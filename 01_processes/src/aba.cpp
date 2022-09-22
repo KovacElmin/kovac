7d2a3b1688d3a0361a1e44045978187dd4700948
Elmin Kovac <elminkov@gmail.com>
2022-09-22 16:54:35 +0200

    aendere das sleep von 1 auf 0.5 Sekundne ab

--- 01_processes/src/aba.cpp
+++ 01_processes/src/aba.cpp
@@ -8,12 +8,12 @@ int main(){
     if(pid == 0){
         while(true){
             cout << "A" << flush;
-            sleep(1);
+            sleep(0.5);
         }
     }else{
         while(true){
             cout << "B" << flush;
-            sleep(1);
+            sleep(0.5);
         }
         
     }

551447d42a6bbcb4fd5d7df9a7a3de60e4056be0
Elmin Kovac <elminkov@gmail.com>
2022-09-24 13:34:06 +0200

    erstelle das charout Programm und aendere das meson.build

---
+++ 01_processes/src/charout.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <chrono>
+#include <thread>
+
+using namespace std;
+
+int main(int argc, char* argv[]){
+    while(true){
+        cout << argv[argc - 1] << flush;
+        chrono::milliseconds sleeptime(500);
+        this_thread::sleep_for(sleeptime);
+    }
+
+}

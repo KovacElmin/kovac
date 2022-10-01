d14a7465b32550cbb4ec3e0310f824e8ab809074
Elmin Kovac <elminkov@gmail.com>
2022-10-01 13:32:48 +0200

    forke zwei Mal und ersetze beide Ausgaben mit charout

--- 01_processes/src/aba.cpp
+++ 01_processes/src/aba.cpp
@@ -5,37 +5,45 @@
 #include <csignal>
 #include <sys/wait.h>
 
+
 using namespace std;
 
-//AM ENDE NOCH KOMMENTARE HINZUFUEGEN
 int main(){
     auto pid{fork()};
-    if(pid == 0){
-        while(true){
-            execl("./charout", "charout", "A", nullptr);
+    if(pid == 0){   
+        execl("./charout", "charout", "A", nullptr);
+        if(errno == -1){
+            cout << "Das Programm charout existiert nicht" << flush;
+            exit(EXIT_FAILURE);
+        }
+            
+    }else{
+        auto pid1{fork()};
+        if(pid1 == 0){
+            execl("./charout", "charout", "B", nullptr);
             if(errno == -1){
-                cout << "Das Programm charout existiert nicht";
+                cout << "Das Programm charout existiert nicht" << flush;
                 exit(EXIT_FAILURE);
             }
+            
         }
-    }else{
-        for(int i = 0; i < 6; i++){
-            cout << "B" << flush;
-            chrono::milliseconds sleeptime(500);
-            this_thread::sleep_for(sleeptime);
-        }
+        sleep(3);
         /*
         Parent Prozess kriegt bei der fork Funktion
         bei der pid Variable die echte process id
         des child Prozesses zurück
         */
-
-       //NUMMER 9 NOCH NICHT .ABA FILE COMMITED
         kill(pid, SIGKILL);
+        kill(pid1, SIGKILL);
         int status;
-        waitpid(pid, &status, 0);
-        cout << "Kindprozess terminiert mit Exit Code "  << status << endl;
-        quick_exit(EXIT_SUCCESS);
         
+        waitpid(pid, &status, 0);
+        cout << "Kindprozess " << pid << " terminiert mit Exit Code "  << WEXITSTATUS(status) << endl;
+        
+        waitpid(pid1, &status, 0);
+        cout << "Kindprozess " << pid1 << " terminiert mit Exit Code "  << WEXITSTATUS(status) << endl;
+        
+        exit(EXIT_SUCCESS);
     }
+        
 }

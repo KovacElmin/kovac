c1083b9f92499177e3a6965e5e5aa233a16c5342
Elmin Kovac <elminkov@gmail.com>
2022-10-07 12:36:59 +0200

    Definiere die Thread Funktion fuer das Auto

--- 02_threads/src/main.cpp
+++ 02_threads/src/main.cpp
@@ -1,5 +1,15 @@
-
+#include <iostream>
+#include <thread>
 using namespace std;
+
+void drive(){
+    int i = 0;
+    while(true){
+        i++;
+        cout << i << " VW Golf" << endl;
+        this_thread::sleep_for(1s);
+    }
+}
 
 int main() {
     return 0;

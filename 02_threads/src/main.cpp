017d513491589abc5075d24020079b9204440cf7
Elmin Kovac <elminkov@gmail.com>
2022-10-12 21:45:14 +0200

    verbessere den Ausgabemechanismus der drive Funktion

--- 02_threads/src/main.cpp
+++ 02_threads/src/main.cpp
@@ -6,9 +6,11 @@ using namespace std;
 
 void drive(string car_name){
     int i = 0;
+    string outputString = "";
     while(true){
         i++;
-        cout << i << " " << car_name << endl;
+        outputString = to_string(i) + " " + car_name;
+        cout << outputString << endl;
         this_thread::sleep_for(1s);
     }
 }

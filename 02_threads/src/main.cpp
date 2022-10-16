28f0c2ff81c4023c6119b903bdcac8b7deb9ecd7
Elmin Kovac <elminkov@gmail.com>
2022-10-16 18:57:53 +0200

    ermittle die Gesamtzeit beider Autos und ermittle den Sieger

--- 02_threads/src/main.cpp
+++ 02_threads/src/main.cpp
@@ -7,12 +7,12 @@ using namespace std;
 
 
 
-void drive(string car_name){
+void drive(string car_name, double& total_time){
     int i = 0;
     string outputString = "";
     ostringstream outputStream;
     double random_time = 0.00;
-    
+
     while(i < 10){
         i++;
         
@@ -21,6 +21,8 @@ void drive(string car_name){
         uniform_real_distribution<> dis{1, 10};
 
         random_time = dis(gen);
+
+        total_time += random_time;
 
         outputStream << to_string(i) <<  " " << car_name 
         << ": " << fixed << setprecision(2) << random_time << "sek";
@@ -46,7 +48,11 @@ public:
     }
 
     void operator()(){
-        drive(this->car_name);
+        drive(this->car_name, ref(this->total_time));
+    }
+
+    double get_total_time(){
+        return total_time;
     }
 
 };
@@ -54,10 +60,20 @@ public:
 
 int main() {
 
-    thread drivingRounds{drive, "VW Golf"};
+    double function_car_time{};
+    thread drivingRounds{drive, "VW Golf", ref(function_car_time)};
     Car c("VW Caddy");
     thread drivingRoundsWithClass{c};
 
     drivingRoundsWithClass.join();
     drivingRounds.join();
+
+    if(c.get_total_time() > function_car_time){
+        cout << "Sieger ist: VW Golf mit " << function_car_time << "sek" << endl;
+        cout << "Verlierer ist: VW Caddy mit " << c.get_total_time() << "sek" << endl;
+    }else{
+        cout << "Sieger ist: VW Caddy mit " << c.get_total_time() << "sek" << endl;
+        cout << "Verlierer ist: VW Golf mit " << function_car_time << "sek" << endl;
+    }
+    
 }

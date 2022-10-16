0f28da5e29982bd01f49ef53be7cbce1b9cb3b0c
Elmin Kovac <elminkov@gmail.com>
2022-10-16 18:36:42 +0200

    zufaellige Rundenzeit wird in eine Variable gespeichert

--- 02_threads/src/main.cpp
+++ 02_threads/src/main.cpp
@@ -11,6 +11,7 @@ void drive(string car_name){
     int i = 0;
     string outputString = "";
     ostringstream outputStream;
+    double random_time = 0.00;
     
     while(true){
         i++;
@@ -19,13 +20,15 @@ void drive(string car_name){
         mt19937 gen{rd()};
         uniform_real_distribution<> dis{1, 10};
 
+        random_time = dis(gen);
+
         outputStream << to_string(i) <<  " " << car_name 
-        << ": " << fixed << setprecision(2) << dis(gen) << "sek";
+        << ": " << fixed << setprecision(2) << random_time << "sek";
         
         cout << outputStream.str() + "\n" << flush;
         outputStream.str("");
 
-        int millisecondsToSleep = dis(gen) * 1000;
+        int millisecondsToSleep = random_time * 1000;
         chrono::milliseconds sleeptime(millisecondsToSleep);
         this_thread::sleep_for(sleeptime);
     }
@@ -35,6 +38,7 @@ void drive(string car_name){
 class Car{
 private:
     string car_name;
+    double total_time;
 
 public:
     Car(string car_name){

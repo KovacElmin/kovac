4a8bad11b681e8f1e46c8fcba7703a07940f56d6
Elmin Kovac <elminkov@gmail.com>
2022-10-30 21:05:24 +0100

    teste alle Funktionen der Klasse aus

--- 03_sync1/src/main.cpp
+++ 03_sync1/src/main.cpp
@@ -6,6 +6,25 @@ using namespace std;
 int main(){
     Account myAccount(20);
 
+    cout << "current balance: " 
+    << myAccount.get_balance() << endl;
 
+    cout << "trying to withdraw 25 Euros: "
+    << myAccount.withdraw(25)
+    << " | current Balance: "
+    << myAccount.get_balance()
+    << endl;
 
+    cout << "trying to withdraw 10 Euros: "
+    << myAccount.withdraw(10)
+    << " | current Balance: "
+    << myAccount.get_balance()
+    << endl;
+
+    cout << "depositing 20 Euros: ";
+    myAccount.deposit(20);
+    cout << " | current Balance: "
+    << myAccount.get_balance()
+    << endl;
+    
 }

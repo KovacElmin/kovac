070c26ce1f639c59b45680c0efd4327a00fd2d05
Elmin Kovac <elminkov@gmail.com>
2022-12-10 14:58:13 +0100

    Boss und Worker erzeugen bzw. verarbeiten die Packete in einer zufaelligen Zeit

--- 04_sync2/src/loadsim.cpp
+++ 04_sync2/src/loadsim.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <thread>
 #include <mutex>
+#include <random>
+#include <iomanip>
 #include "work_packet.h"
 #include "work_queue.h"
 
@@ -11,9 +13,18 @@ void worker(int id, WorkQueue& q){
         cout << "W" << id << ": Want work packet" << endl;
         WorkPacket wp = q.pop();
         cout << "W" << id << ": Got work packet " << wp.getId() << endl;
-        chrono::milliseconds sleeptime(1000);
+        
+        random_device rd;
+        mt19937 gen{rd()};
+        uniform_real_distribution<double> dis{1, 10};
+
+        double random_time = dis(gen);
+        int millisecondsToSleep = random_time * 1000;
+
+        chrono::milliseconds sleeptime(millisecondsToSleep);
         this_thread::sleep_for(sleeptime);
-        cout << "W" << id << ": Processed work packet " << wp.getId() << endl;
+
+        cout << "W" << id << ": Processed work packet " << wp.getId() <<  "(" << setprecision(2) << random_time << "s)" << endl;
     }
 
 }
@@ -24,14 +35,25 @@ int main(){
     
     thread worker1{worker, 1, ref(wq)};
     thread worker2{worker, 2, ref(wq)};
+    thread worker3{worker, 3, ref(wq)};
 
 
     while(true){
+        random_device rd;
+        mt19937 gen{rd()};
+        uniform_real_distribution<double> dis{0, 1};
+
+        double random_time = dis(gen);
+        int millisecondsToSleep = random_time * 1000;
+        
+        cout << "B: Waiting to submit work packet " << i << endl;
+        
+        chrono::milliseconds sleeptime(millisecondsToSleep);
+        this_thread::sleep_for(sleeptime);
+
         WorkPacket wp(i);
         wq.push(wp);
-        cout << "B: Submitted work packet " << i << endl;
-        chrono::milliseconds sleeptime(500);
-        this_thread::sleep_for(sleeptime);
+        cout << "B: Submitted work packet " << i <<  "(" << setprecision(2) << random_time << "s)" << endl;
         i++;
     }
 

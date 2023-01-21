267b618d2122366b708995e73bd8d6c5c6c10290
Elmin Kovac <elminkov@gmail.com>
2023-01-21 11:10:44 +0100

    erstelle die TimeSlave Klasse

--- 07_berkeley/src/main.cpp
+++ 07_berkeley/src/main.cpp
@@ -1,9 +1,25 @@
 #include "clock.h"
 #include <iostream>
+#include <string>
 #include <thread>
 
 using namespace std;
+
+class TimeSlave{
+private:
+    string name_;
+    int hours_, minutes_, seconds_;
+public:
+    TimeSlave(string name_, int hours_, int minutes_, int seconds_)
+    : name_(name_), hours_(hours_), minutes_(minutes_), seconds_(seconds_){}
+    
+    void operator()(){
+        Clock myClock(name_, hours_, minutes_, seconds_);
+    }
+};
+
 int main(){
     thread clock{Clock("testclock", 15, 45, 0)};
     clock.join();
+    
 }

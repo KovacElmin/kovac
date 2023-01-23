43f3a67338da1d5918a629e23c6fe2b0b229f984
Elmin Kovac <elminkov@gmail.com>
2023-01-23 08:02:54 +0100

    erstelle slave1 und slave2, die beide eine Clock starten

--- 07_berkeley/src/clock.cpp
+++ 07_berkeley/src/clock.cpp
@@ -3,23 +3,28 @@
 #include <chrono>
 #include <iostream>
 #include <thread>
+#include <mutex>
 
 
 using namespace std;
 
 void Clock::operator()(){
     int passedSeconds = 0;
+    ostringstream outputStream;
     while (true)
     {
         this_thread::sleep_for(std::chrono::milliseconds(1000));
         
-
         chrono::system_clock systemClock;
         chrono::time_point<chrono::system_clock> currTime = systemClock.now();
         chrono::time_point<chrono::system_clock> editedTime = TIME_UTILS_H::set_time(currTime, hours_, minutes_, seconds_);
 
         editedTime += chrono::seconds(passedSeconds);
-        cout << name_ << ": " << editedTime << endl;
+        
+        outputStream << name_ << ": " << editedTime;
+        cout << outputStream.str() + "\n" << flush;
+        outputStream.str("");
+        
         passedSeconds++;
     }
     
--- 07_berkeley/src/main.cpp
+++ 07_berkeley/src/main.cpp
@@ -14,12 +14,16 @@ public:
     : name_(name_), hours_(hours_), minutes_(minutes_), seconds_(seconds_){}
     
     void operator()(){
-        Clock myClock(name_, hours_, minutes_, seconds_);
+        thread clock(Clock(name_, hours_, minutes_, seconds_));
+        clock.join();
     }
 };
 
 int main(){
-    thread clock{Clock("testclock", 15, 45, 0)};
-    clock.join();
+    thread slave1{TimeSlave("slave1", 15, 45, 0)};
+    thread slave2{TimeSlave("slave2", 20, 25, 0)};
+
+    slave1.join();
+    slave2.join();
     
 }

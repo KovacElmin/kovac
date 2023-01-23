62fd01b916bf43563e0347b752347cc8ad79b490
Elmin Kovac <elminkov@gmail.com>
2023-01-23 09:10:42 +0100

    Fuege zur TimeSlave Klasse einen Channel und den dazugehoerigen getter hinzu

--- 07_berkeley/src/main.cpp
+++ 07_berkeley/src/main.cpp
@@ -10,6 +10,7 @@ class TimeSlave{
 private:
     string name_;
     int hours_, minutes_, seconds_;
+    Channel channel;
 public:
     TimeSlave(string name_, int hours_, int minutes_, int seconds_)
     : name_(name_), hours_(hours_), minutes_(minutes_), seconds_(seconds_){}
@@ -17,6 +18,10 @@ public:
     void operator()(){
         thread clock(Clock(name_, hours_, minutes_, seconds_));
         clock.join();
+    }
+
+    Channel* get_channel(){
+        return &channel;
     }
 };
 

d1d549032d79187aad7d2fa47cffd0f693cc3e57
Elmin Kovac <elminkov@gmail.com>
2023-01-25 15:59:09 +0100

    Verbessere die Funktionen in pipe.h

--- 07_berkeley/include/pipe.h
+++ 07_berkeley/include/pipe.h
@@ -14,7 +14,7 @@ class Pipe {
   public:
     Pipe& operator<<(T value) {
         if(!closed){
-            unique_lock<mutex> guard{mtx};
+            std::unique_lock<std::mutex> guard{mtx};
             backend.push(value);
             not_empty.notify_one();
         }
@@ -23,8 +23,8 @@ class Pipe {
     
     Pipe& operator>>(T& value) {
         if(!closed){
-            unique_lock<mutex> guard{mtx};
-            not_empty.wait(guard, [this]{ return myQueue.size();});
+            std::unique_lock<std::mutex> guard{mtx};
+            not_empty.wait(guard, [this]{ return backend.size();});
             value = backend.front();
             backend.pop();
         }
@@ -32,12 +32,12 @@ class Pipe {
     }
 
     void close() {
-        unique_lock<mutex> guard{mtx};
+        std::unique_lock<std::mutex> guard{mtx};
         closed = true;
     }
     
     explicit operator bool() {
-        unique_lock<mutex> guard{mtx};
+        std::unique_lock<std::mutex> guard{mtx};
         return !closed;
     }
 };

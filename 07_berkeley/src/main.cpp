28e10d50930ea6d781f4b04fca20040e0c916683
Elmin Kovac <elminkov@gmail.com>
2023-01-25 16:00:44 +0100

    Uebergebe Werte in die Pipes der Slaves mit dem Master, die Slaves geben diese dann aus

--- 07_berkeley/src/main.cpp
+++ 07_berkeley/src/main.cpp
@@ -1,43 +1,13 @@
 #include "clock.h"
 #include "pipe.h"
+#include <chrono>
 #include <iostream>
-#include <string>
 #include <thread>
+#include <mutex>
+#include <sstream>
 
 using namespace std;
 
-class TimeSlave{
-private:
-    string name_;
-    int hours_, minutes_, seconds_;
-    Channel channel;
-public:
-    TimeSlave(string name_, int hours_, int minutes_, int seconds_)
-    : name_(name_), hours_(hours_), minutes_(minutes_), seconds_(seconds_){}
-    
-    void operator()(){
-        thread clock(Clock(name_, hours_, minutes_, seconds_));
-        clock.join();
-    }
-
-    Channel* get_channel(){
-        return &channel;
-    }
-};
-
-class TimeMaster{
-private:
-    string name_;
-    int hours_, minutes_, seconds_;
-public:
-    TimeMaster(string name_, int hours_, int minutes_, int seconds_)
-    : name_(name_), hours_(hours_), minutes_(minutes_), seconds_(seconds_){}
-    
-    void operator()(){
-        thread clock(Clock(name_, hours_, minutes_, seconds_));
-        clock.join();
-    }
-};
 
 class Channel{
 private:
@@ -53,14 +23,98 @@ public:
     }
 };
 
+class TimeSlave{
+private:
+    string name_;
+    int hours_, minutes_, seconds_;
+    Channel* channel;
+public:
+    TimeSlave(string name_, int hours_, int minutes_, int seconds_)
+    : name_(name_), hours_(hours_), minutes_(minutes_), seconds_(seconds_){}
+    
+    void operator()(){
+        /*
+        thread clock(Clock(name_, hours_, minutes_, seconds_));
+        clock.join();
+        */
+        long value{};
+        ostringstream outputStream;
+        while(channel->get_pipe1() >> value){
+            outputStream << name_ << " " << value;
+            cout << outputStream.str() << "\n" << flush;
+            outputStream.str("");
+        }
+
+    }
+
+    void set_channel(Channel* ch){
+        channel = ch;
+    }
+
+    Channel* get_channel(){
+        return channel;
+    }
+};
+
+class TimeMaster{
+private:
+    string name_;
+    int hours_, minutes_, seconds_;
+    Channel* channel1;
+    Channel* channel2;
+public:
+    TimeMaster(string name_, int hours_, int minutes_, int seconds_)
+    : name_(name_), hours_(hours_), minutes_(minutes_), seconds_(seconds_){}
+    
+    void operator()(){
+        /*
+        thread clock(Clock(name_, hours_, minutes_, seconds_));
+        clock.join();
+        */
+        channel1->get_pipe1() << 1;
+        channel1->get_pipe1() << 2;
+        channel1->get_pipe1() << 3;
+
+        channel2->get_pipe1() << 1;
+        channel2->get_pipe1() << 2;
+        channel2->get_pipe1() << 3;
+
+        this_thread::sleep_for(chrono::milliseconds(500));
+
+        channel1->get_pipe1().close();
+        channel2->get_pipe2().close();
+    }
+
+    void set_channel1(Channel* ch1){
+        channel1 = ch1;
+    }
+
+    void set_channel2(Channel* ch2){
+        channel2 = ch2;
+    }
+};
+
+
 int main(){
-    thread master{TimeMaster("master", 21, 0, 0)};
+    Channel ch1;
+    Channel ch2;
 
-    thread slave1{TimeSlave("slave1", 15, 45, 0)};
-    thread slave2{TimeSlave("slave2", 20, 25, 0)};
+    TimeMaster master("master", 21, 0, 0);
+    master.set_channel1(&ch1);
+    master.set_channel2(&ch2);
 
-    master.join();
-    slave1.join();
-    slave2.join();
+    TimeSlave slave1("slave1", 15, 45, 0);
+    slave1.set_channel(&ch1);
+
+    TimeSlave slave2("slave2", 20, 25, 0);
+    slave2.set_channel(&ch2);
+
+    thread master_thread{master};
+    thread slave1_thread{slave1};
+    thread slave2_thread{slave2};
+
+    master_thread.join();
+    slave1_thread.join();
+    slave2_thread.join();
     
 }

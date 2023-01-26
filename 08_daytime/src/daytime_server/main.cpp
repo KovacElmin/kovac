b5007ed63fbfd36ab92a778670330ffb6eca8c72
Elmin Kovac <elminkov@gmail.com>
2023-01-26 19:26:38 +0100

    Server schliesst die Connection nicht mehr nach nur einer Abfrage

--- 08_daytime/src/daytime_server/main.cpp
+++ 08_daytime/src/daytime_server/main.cpp
@@ -7,29 +7,24 @@ using namespace std;
 using namespace asio::ip;
 
 int main() {
-    asio::io_context ctx;
-    tcp::endpoint ep{tcp::v4(), 1113};
-    tcp::acceptor acceptor{ctx, ep};
-    acceptor.listen();
+    while(true){
+        asio::io_context ctx;
+        tcp::endpoint ep{tcp::v4(), 1113};
+        tcp::acceptor acceptor{ctx, ep};
+        acceptor.listen();
 
-    tcp::socket sock{ctx};
-    acceptor.accept(sock);
-    tcp::iostream strm{std::move(sock)};
+        tcp::socket sock{ctx};
+        acceptor.accept(sock);
+        tcp::iostream strm{std::move(sock)};
 
-    std::time_t t = std::chrono::system_clock::to_time_t(chrono::system_clock::now());
-    std::string ts = std::ctime(&t);
-    ts.resize(ts.size()-1);
+        std::time_t t = std::chrono::system_clock::to_time_t(chrono::system_clock::now());
+        std::string ts = std::ctime(&t);
+        ts.resize(ts.size()-1);
 
-    /*
+
+        TIME_UTILS_H::operator<<(strm, chrono::system_clock::now());
+
+        strm.close();
+    }
     
-inline std::ostream& operator<<(std::ostream& out, decltype(std::chrono::system_clock::now()) time) {
-    std::time_t t{std::chrono::system_clock::to_time_t(time)};
-    out << std::put_time(std::localtime(&t), "%Y-%m-%d %H:%M:%S");
-    return out;
-}
-    */
-
-    TIME_UTILS_H::operator<<(strm, chrono::system_clock::now());
-
-    strm.close();
 }

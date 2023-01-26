6e186e80d3f4803c824d2f2a2d8535e64cda6419
Elmin Kovac <elminkov@gmail.com>
2023-01-26 19:24:40 +0100

    Schreibe jetzt meinen eigenen Server der die aktuelle Zeit zurueck gibt

--- 08_daytime/src/daytime_server/main.cpp
+++ 08_daytime/src/daytime_server/main.cpp
@@ -1,7 +1,35 @@
-#include <iostream>
+#include "timeutils.h"
+#include <iostream> 
+#include <asio.hpp>
+#include <chrono>
 
-using namespace std;
+using namespace std; 
+using namespace asio::ip;
 
-int main(){
+int main() {
+    asio::io_context ctx;
+    tcp::endpoint ep{tcp::v4(), 1113};
+    tcp::acceptor acceptor{ctx, ep};
+    acceptor.listen();
+
+    tcp::socket sock{ctx};
+    acceptor.accept(sock);
+    tcp::iostream strm{std::move(sock)};
+
+    std::time_t t = std::chrono::system_clock::to_time_t(chrono::system_clock::now());
+    std::string ts = std::ctime(&t);
+    ts.resize(ts.size()-1);
+
+    /*
     
+inline std::ostream& operator<<(std::ostream& out, decltype(std::chrono::system_clock::now()) time) {
+    std::time_t t{std::chrono::system_clock::to_time_t(time)};
+    out << std::put_time(std::localtime(&t), "%Y-%m-%d %H:%M:%S");
+    return out;
+}
+    */
+
+    TIME_UTILS_H::operator<<(strm, chrono::system_clock::now());
+
+    strm.close();
 }
